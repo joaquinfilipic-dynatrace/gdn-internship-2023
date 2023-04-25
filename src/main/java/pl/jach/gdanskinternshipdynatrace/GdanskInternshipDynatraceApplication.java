a2dfc65f26d48fcb2e80fc13543b1465d1c38e19
Kacper Jach <kacperini@gmail.com>
2023-04-25 21:08:34 +0200

    GID-000 Initial commit

---
+++ src/main/java/pl/jach/gdanskinternshipdynatrace/GdanskInternshipDynatraceApplication.java
@@ -0,0 +1,13 @@
+package pl.jach.gdanskinternshipdynatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdanskInternshipDynatraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdanskInternshipDynatraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jach/gdanskinternshipdynatrace/GdanskInternshipDynatraceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jach.gdanskinternshipdynatrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdanskInternshipDynatraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
